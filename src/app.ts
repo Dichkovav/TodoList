168d744b08a09ab07280dcad58b8349c8c71b8ad
Venelina Dichkova <vcdichkova@gmail.com>
2020-09-10 14:15:02 +0100

    Todo

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express, { Express } from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import todoRoutes from "./routes"
+
+const app: Express = express()
+
+const PORT: string | number = process.env.PORT || 4000
+
+app.use(cors())
+app.use(todoRoutes)
+
+const uri: string = `mongodb+srv://veni123:<veni>@cluster0.aky40.mongodb.net/<Todo>?retryWrites=true&w=majority`
+const options = { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.set("useFindAndModify", false)
+
+mongoose
+  .connect(uri, options)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`)
+    )
+  )
+  .catch(error => {
+    throw error
+  })
